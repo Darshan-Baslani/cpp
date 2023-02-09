98741bebed5692ecc8feb8dd13e84301e0f6720c
Darshan-Baslani <dcbaslani@gmail.com>
2023-02-09 07:44:51 +0530

    c++

---
+++ armStrong.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+bool armStrong(int n, int &ans) {
+    if(n<=0)
+        return true;
+    int rem = n%10;
+    ans += rem*rem*rem;
+    armStrong(n/10,ans);
+    return ans==n?true:false;
+}
+
+int main() {
+
+    int n;
+    cout<<"Enter any no: ";
+    cin>>n;
+    int ans = 0;
+    cout<<armStrong(n,ans);
+
+    return 0;
+}
