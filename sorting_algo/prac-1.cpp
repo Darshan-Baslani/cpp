4ad7ae4ce0513a5b7ce948e5b7d5a9ae7c91fe4c
Darshan-Baslani <dcbaslani@gmail.com>
2023-03-10 10:20:35 +0530

    c++

---
+++ sorting_algo/prac-1.cpp
@@ -0,0 +1,29 @@
+//given an integer array arr, move all 0's to end of it while maintaning the relative order of non 0's element
+//input = 050342   output = 534200 
+#include<iostream>
+using namespace std;
+
+void f(int arr[], int n) {
+    for(int i=1; i<n; i++) {
+        int temp = arr[i];
+        int j=i-1;
+        while(j>=0 and arr[j] == 0) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+}
+
+int main() {
+
+    int arr[] = {0,5,0,3,4,2},n=6;
+
+    f(arr,n);
+
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
