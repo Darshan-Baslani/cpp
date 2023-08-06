eaaf645521b2e64f0656f209197659358d3c5f72
Darshan <dcbaslani@gmail.com>
2023-08-06 15:51:22 +0530

    OOP

--- OOP/prac-1.cpp
+++ OOP/prac-1.cpp
@@ -6,17 +6,41 @@ public:
     int length;
     int breath;
 
+    //Default Constructor - No Parameters
     Rectangle() {
         length = 0;
         breath = 0;
+    }
+
+    //Paramaterized Constructor
+    Rectangle(int x, int y) {
+        length = x;
+        breath = y;
+    }
+
+    //Copy Constructor
+    Rectangle(Rectangle& r) {
+        length = r.length;
+        breath = r.breath;
+    }
+
+    ~Rectangle(){
+        cout<<"Destructor is Called!"<<endl;
     }
 
 };
 
 int main() {
 
-    Rectangle r;
-    cout<<r.length<<" "<<r.breath<<endl;
+    Rectangle* r = new Rectangle;
+    cout<<r->length<<" "<<r->breath<<endl; // In pointer objects, arrow should be used instead of dot
+    delete r; // delete could only be used in pointers.
+
+    Rectangle r2(2, 4);
+    cout<<r2.length<<" "<<r2.breath<<endl;
+
+    Rectangle r3(r2);
+    cout<<r3.length<<" "<<r3.breath<<endl;
 
     return 0;
 }
