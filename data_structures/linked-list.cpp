80fa0960dd1a980ca8872e8db1dd382bfff40337
Darshan-Baslani <darshan.baslani4@gmail.com>
2024-04-29 10:23:50 +0530

    linked list updated

--- data_structures/linked-list.cpp
+++ data_structures/linked-list.cpp
@@ -11,10 +11,34 @@ public:
   }
 };
 
-void insertion_at_head(Node *head, int value) {
+void insertion_at_head(Node *&head, int value) {
   Node *new_node = new Node(value);
   new_node->next = head;
   head = new_node;
+}
+
+void insertion_at_tail(Node *head, int value) {
+  Node *new_node = new Node(value);
+  new_node->next = NULL;
+  Node *temp = head;
+  while (temp->next != NULL) {
+    temp = temp->next;
+  }
+  temp->next = new_node;
+}
+
+void insertion_at_k(Node *&head, int k, int value) {
+  if (k == 0) {
+    insertion_at_head(head, value);
+    return;
+  }
+  Node *new_node = new Node(value);
+  Node *temp = head;
+  for (int i = 0; i < k - 1; i++) {
+    temp = temp->next;
+  }
+  new_node->next = temp->next;
+  temp->next = new_node;
 }
 
 void display(Node *head) {
@@ -29,6 +53,9 @@ void display(Node *head) {
 int main() {
   Node *head = new Node(10);
   insertion_at_head(head, 9);
+  insertion_at_tail(head, 11);
+  insertion_at_k(head, 3, 13);
+
   display(head);
   return 0;
 }
