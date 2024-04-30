8fae19b2b88294ecb8b11616ed7251f1795b4c7e
Darshan-Baslani <darshan.baslani4@gmail.com>
2024-04-30 10:04:03 +0530

    linked list updated

--- data_structures/linked-list.cpp
+++ data_structures/linked-list.cpp
@@ -41,6 +41,36 @@ void insertion_at_k(Node *&head, int k, int value) {
   temp->next = new_node;
 }
 
+void update_at_k(Node *head, int k, int value) {
+  Node *temp = head;
+  for (int i = 0; i < k - 1; i++) {
+    temp = temp->next;
+  }
+  temp->val = value;
+}
+
+void delete_at_head(Node *&head) { head = head->next; }
+
+void delete_at_tail(Node *&head) {
+  Node *temp = head;
+  while (temp->next->next != NULL) {
+    temp = temp->next;
+  }
+  temp->next = NULL;
+}
+
+void delete_at_k(Node *head, int k) {
+  if (k == 0)
+    delete_at_head(head);
+  Node *temp = head;
+  for (int i = 0; i < k - 2; i++) {
+    temp = temp->next;
+  }
+  Node *temp2 = temp;
+  temp = temp->next;
+  temp2->next = temp->next;
+}
+
 void display(Node *head) {
   Node *temp = head;
   while (temp != NULL) {
@@ -53,8 +83,20 @@ void display(Node *head) {
 int main() {
   Node *head = new Node(10);
   insertion_at_head(head, 9);
+  insertion_at_head(head, 7);
+  insertion_at_head(head, 6);
   insertion_at_tail(head, 11);
   insertion_at_k(head, 3, 13);
+  display(head);
+
+  update_at_k(head, 3, 8);
+  display(head);
+
+  delete_at_head(head);
+  display(head);
+  delete_at_tail(head);
+  display(head);
+  delete_at_k(head, 1);
 
   display(head);
   return 0;
