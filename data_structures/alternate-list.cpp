59e275db85992aa18fe54fc69e0b9d15989f6003
Darshan-Baslani <darshan.baslani4@gmail.com>
2024-05-03 09:06:09 +0530

    delete atlternate element in list

---
+++ data_structures/alternate-list.cpp
@@ -0,0 +1,68 @@
+// give a linked list, delete every alternate element starting from second
+// element
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+  int val;
+  Node *next;
+  Node() {}
+  Node(int value) { val = value; }
+};
+
+void insert_at_tail(Node *head, int value) {
+  Node *new_node = new Node(value);
+  if (head == NULL) {
+    head = new_node;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != NULL) {
+    temp = temp->next;
+  }
+  temp->next = new_node;
+  new_node->next = NULL;
+}
+
+void display(Node *head) {
+  Node *temp = head;
+  while (temp != NULL) {
+    cout << temp->val << " -> ";
+    temp = temp->next;
+  }
+  cout << endl;
+}
+
+void remove_alternate(Node *&head) {
+  int i = 0;
+  Node *temp = head;
+  while (temp->next != NULL) {
+    if (i % 2 == 0) {
+      temp->next = temp->next->next;
+    } else {
+      temp = temp->next;
+    }
+    i++;
+  }
+}
+
+int main() {
+  Node *head = new Node(11);
+  insert_at_tail(head, 22);
+  insert_at_tail(head, 33);
+  insert_at_tail(head, 44);
+  insert_at_tail(head, 55);
+  insert_at_tail(head, 66);
+  insert_at_tail(head, 77);
+  insert_at_tail(head, 88);
+  insert_at_tail(head, 99);
+
+  display(head);
+
+  remove_alternate(head);
+
+  display(head);
+
+  return 0;
+}
