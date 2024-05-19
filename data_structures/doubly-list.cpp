c54d592f3d955a2799224548eb3e5f95f8625e9c
Darshan <dcbaslani@gmail.com>
2024-05-19 20:47:36 +0530

    .

--- data_structures/doubly-list.cpp
+++ data_structures/doubly-list.cpp
@@ -5,12 +5,12 @@ class Node {
 public:
   int val;
   Node *next;
-  Node *tail;
+  Node *prev;
 
   Node(){} 
   Node(int value){
     val = value;
-    tail = NULL;
+    prev = NULL;
     next = NULL;
   }
 };
@@ -24,7 +24,7 @@ void display(Node *head) {
   cout << endl;
 }
 
-void insertion_at_tail(Node *&head, int value) {
+void insertion_at_prev(Node *&head, int value) {
   Node *new_node = new Node(value);
   if(head == NULL) {
     head = new_node;
@@ -38,14 +38,39 @@ void insertion_at_tail(Node *&head, int value) {
   temp->next = new_node;
 }
 
+void insertion_at_head(Node *&head, int value) {
+  Node *new_node = new Node(value);
+  head->prev = new_node;
+  new_node->next = head;
+  head = new_node;
+}
+
+void insertion_at_k(Node *&head, int value, int k) {
+  if(k == 0) {
+    insertion_at_head(head, value);
+    return;
+  }
+  Node *new_node = new Node(value);
+  Node *temp = head;
+  for(int i=0; i<=k; i++) {
+    temp = temp->next;
+  }
+  new_node->prev = temp->prev;
+  new_node->next = temp;
+  temp->prev = new_node;
+}
+
 int main() {
   Node *head = NULL;
-  insertion_at_tail(head, 11);
-  insertion_at_tail(head, 12);
-  insertion_at_tail(head, 13);
-  insertion_at_tail(head, 14);
-  insertion_at_tail(head, 15);
+  insertion_at_prev(head, 11);
+  insertion_at_prev(head, 12);
+  insertion_at_prev(head, 13);
+  insertion_at_prev(head, 14);
+  insertion_at_prev(head, 15);
+  insertion_at_head(head, 10);
   display(head);
 
+  insertion_at_k(head, 20, 3);
+  display(head);
   return 0;
 }
