cefb7b326866fa27056ae92d11efab17c3fb4f5a
Darshan <dcbaslani@gmail.com>
2024-06-11 23:02:10 +0530

    .

--- data_structures/hashing.cpp
+++ data_structures/hashing.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<list>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
 class Hashing {
+  public:
   vector<list<int>> hashtable;
   int size;
 
@@ -17,23 +19,36 @@ class Hashing {
     return key%size;
   }
 
-  void add(int value) {
+  void add_val(int value) {
     int hash_value = hashvalue(value);
     hashtable[hash_value].push_back(value);  
   }
 
-  list<int>::iterator search(int value) {
+  list<int>::iterator search_val(int value) {
     int hash_value = hashvalue(value);
     return find(hashtable[hash_value].begin(), hashtable[hash_value].end(), value);
   }
   
-  void delete(int value) {
+  void delete_val(int value) {
     int hash_value = hashvalue(value);
-    hashtable[hash_value].erase(search(value));
+    if(search_val(value) != hashtable[hash_value].end()){
+      hashtable[hash_value].erase(search_val(value));
+      cout<<value<<" is deleted\n";
+    }else{
+      cout<<value<<" is not present\n";
+    }
   }
 };
 
 int main() {
+
+  Hashing hash = Hashing(10);
+  hash.add_val(10);
+  hash.add_val(11);
+  hash.add_val(12);
+
+  hash.delete_val(11);
+  hash.delete_val(11);
 
   return 0;
 }
